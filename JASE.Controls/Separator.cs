771e56764281cf04f46b4a2f1c95308b15595560
jason-pezzimenti <146531763+jason-pezzimenti@users.noreply.github.com>
2024-04-27 16:42:32 +1000

    Add project files.

---
+++ JASE.Controls/Separator.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JASE.Controls
+{
+	public partial class Separator : Control
+	{
+		[Browsable(true), DisplayName("Using Gradient"), Category("Behavior"), Description("Gets or sets whether to use a Gradient.")]
+		public bool UsingGradient { get; set; }
+		[Browsable(true), DisplayName("Is Enabled"), Category("Behavior"), Description("Gets or sets whether the Separator is Enabled.")]
+		public bool IsEnabled { get; set; } = true;
+		[Browsable(true), DisplayName("Is Dashed"), Category("Behavior"), Description("Gets or sets whether the Line is dashed.")]
+		public bool IsDashed { get; set; }
+		[Browsable(true), DisplayName("Dash Style"), Category("Appearance"), Description("Gets or sets the Dash Style for this Control.")]
+		public DashStyle Style { get; set; }
+		[Browsable(true), DisplayName("Dash Pattern"), Category("Appearance"), Description("Gets or sets the Dash Pattern for this Dashed Line.")]
+		public float[] DashPattern { get; set; }
+
+		public enum Directions
+		{
+			Horizontal = 0,
+			Vertical
+		}
+
+		[Browsable(true), DisplayName("Direction"), Category("Behavior"), Description("Gets or sets the Direction for this Line.")]
+		public Directions Direction { get; set; }
+		[Browsable(true), DisplayName("Start Color"), Category("Appearance"), Description("Gets or sets the Starting Color for this Gradient Line.")]
+		
+		public Color StartColor { get; set; }
+
+		[Browsable(true), DisplayName("End Color"), Category("Appearance"), Description("Gets or sets the Ending Color for this Gradient Line.")]
+		public Color EndColor { get; set; }
+
+		[Browsable(true), DisplayName("Line Thickness"), Category("Appearance"), Description("Gets or sets the Thickness for this Line.")]
+		public int Thickness { get; set; } = 1;
+
+		[Browsable(true), DisplayName("Color"), Category("Appearance"), Description("Gets or sets the Color for this Line.")]
+		public Color Color { get; set; }
+
+		public Separator()
+		{
+			Thickness = 1;
+
+			InitializeComponent();
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+			switch (Direction)
+			{
+				case Directions.Horizontal:
+					if(IsEnabled)
+					{
+						if (UsingGradient)
+						{
+							LinearGradientBrush linearGradientBrush = new LinearGradientBrush(
+								new Point(
+									0,
+									0
+									),
+								new Point(
+									this.Width,
+									0
+									),
+								StartColor,
+								EndColor
+								);
+
+							using (Pen pen = new Pen(
+								linearGradientBrush,
+								(float)Thickness
+								))
+							{
+								if(IsDashed)
+								{
+									pen.DashStyle = Style;
+									pen.DashPattern = DashPattern;
+								}
+
+								e.Graphics.DrawLine(
+									pen,
+									0,
+									0,
+									this.Width,
+									0
+									);
+							}
+						}
+						else
+						{
+							using (Pen pen = new Pen(
+							Color,
+							Thickness
+							)
+							)
+							{
+								if (IsDashed)
+								{
+									pen.DashStyle = Style;
+									pen.DashPattern = DashPattern;
+								}
+
+								e.Graphics.DrawLine(
+									pen,
+									new Point(
+										0,
+										0
+										),
+									new Point(
+										this.Width,
+										0
+										)
+									);
+							}
+						}
+					}
+					else
+					{
+						using (Pen pen = new Pen(
+							Brushes.DimGray,
+							Thickness
+							)
+							)
+						{
+							if (IsDashed)
+							{
+								pen.DashStyle = Style;
+								pen.DashPattern = DashPattern;
+							}
+
+							e.Graphics.DrawLine(
+								pen,
+								new Point(
+									0,
+									0
+									),
+								new Point(
+									0,
+									this.Height
+									)
+								);
+						}
+					}
+					break;
+				case Directions.Vertical:
+					if (IsEnabled)
+					{
+						if (UsingGradient)
+						{
+							LinearGradientBrush linearGradientBrush = new LinearGradientBrush(
+								new Point(
+									0,
+									0
+									),
+								new Point(
+									0,
+									this.Height
+									),
+								StartColor,
+								EndColor
+								);
+
+							using (Pen pen = new Pen(
+								linearGradientBrush,
+								(float)Thickness
+								))
+							{
+								if (IsDashed)
+								{
+									pen.DashStyle = Style;
+									pen.DashPattern = DashPattern;
+								}
+
+								e.Graphics.DrawLine(
+									pen,
+									0,
+									0,
+									0,
+									this.Height
+									);
+							}
+						}
+						else
+						{
+							using (Pen pen = new Pen(
+							Color,
+							Thickness
+							)
+							)
+							{
+								if (IsDashed)
+								{
+									pen.DashStyle = Style;
+									pen.DashPattern = DashPattern;
+								}
+
+								e.Graphics.DrawLine(
+									pen,
+									new Point(
+										0,
+										0
+										),
+									new Point(
+										0,
+										this.Height
+										)
+									);
+							}
+						}
+					}
+					else
+					{
+						using (Pen pen = new Pen(
+							Brushes.DimGray,
+							Thickness
+							)
+							)
+						{
+							if (IsDashed)
+							{
+								pen.DashStyle = Style;
+								pen.DashPattern = DashPattern;
+							}
+
+							e.Graphics.DrawLine(
+								pen,
+								new Point(
+									0,
+									0
+									),
+								new Point(
+									0,
+									this.Height
+									)
+								);
+						}
+					}
+					break;
+			}
+
+			base.OnPaint(e);
+		}
+
+		private void Separator_SizeChanged(object sender, EventArgs e)
+		{
+			this.Refresh();
+		}
+	}
+}
---
+++ Test Project/Window.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Test_Project
+{
+	partial class Window
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.separator1 = new JASE.Controls.Separator();
+			this.SuspendLayout();
+			// 
+			// separator1
+			// 
+			this.separator1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.separator1.Color = System.Drawing.Color.Empty;
+			this.separator1.DashPattern = null;
+			this.separator1.Direction = JASE.Controls.Separator.Directions.Horizontal;
+			this.separator1.EndColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.separator1.IsDashed = false;
+			this.separator1.IsEnabled = true;
+			this.separator1.Location = new System.Drawing.Point(13, 13);
+			this.separator1.Name = "separator1";
+			this.separator1.Size = new System.Drawing.Size(225, 1);
+			this.separator1.StartColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+			this.separator1.Style = System.Drawing.Drawing2D.DashStyle.Solid;
+			this.separator1.TabIndex = 0;
+			this.separator1.Text = "separator1";
+			this.separator1.Thickness = 1;
+			this.separator1.UsingGradient = true;
+			// 
+			// Window
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(250, 351);
+			this.Controls.Add(this.separator1);
+			this.Name = "Window";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private JASE.Controls.Separator separator1;
+	}
+}
+
